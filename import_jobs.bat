010654b974a7def47cfbb0f9e0badd46c00d09aa
Roberto Bertaccini <roberto.bertaccini@gmail.com>
2025-05-08 18:07:29 +0200

    Delete import_jobs.bat

--- import_jobs.bat
+++
@@ -1,4 +0,0 @@
-java -jar jenkins-cli.jar -s http://admin:bertax@localhost:1111 create-job "TellUsMore POC All Browsers" < "jobs\TellUsMore POC All Browsers\config.xml"
-java -jar jenkins-cli.jar -s http://admin:bertax@localhost:1111 create-job ImportedJob < "jobs\TellUsMore POC All Browsers\config.xml"
-java -jar jenkins-cli.jar -s http://admin:bertax@localhost:1111 create-job ImportedJob < "jobs\TellUsMore POC All Browsers\config.xml"
-java -jar jenkins-cli.jar -s http://admin:bertax@localhost:1111 create-job ImportedJob < "jobs\TellUsMore POC All Browsers\config.xml"
